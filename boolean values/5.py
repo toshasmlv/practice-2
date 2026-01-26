b135326fc5a01371f566bd4de694b8dcce37a843
toshasmlv <t_musirkhan@kbtu.kz>
2026-01-26 12:09:18 +0500

    added examples boolean operators

--- boolean values/5.py
+++ boolean values/5.py
@@ -1,2 +1,3 @@
+#Python also has many built-in functions that return a boolean value, like the isinstance() function, which can be used to determine if an object is of a certain data type:
 x = 200
 print(isinstance(x, int))
